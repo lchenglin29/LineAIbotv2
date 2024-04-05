891cf29749685f9b4872cc5ddc7abf88ab3f3832
lchenglin29 <137890151+lchenglin29@users.noreply.github.com>
2024-04-05 15:43:25 +0000

    嘗試環境get

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 import requests
 
 
-GEMINI_API_KEY = os.environ['api_key']
+GEMINI_API_KEY = os.environ.get('api_key')
 
 def calling_gemini_api(data):
     url = f'https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key={GEMINI_API_KEY}'
