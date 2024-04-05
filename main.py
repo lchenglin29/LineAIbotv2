8a5304b9fb65d2dfed1819cd1d7e04562d243525
lchenglin29 <137890151+lchenglin29@users.noreply.github.com>
2024-04-05 18:00:42 +0000

    圖片功能

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import random
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage,ImageMessage
 import json
 import requests
+import base64
 
 
 GEMINI_API_KEY = os.environ['api_key']
@@ -18,6 +19,31 @@ def calling_gemini_api(data):
       print(response.json())
       return response.json()
     else:
+      return "Error"
+
+def calling_gemini_vision_api(text, image_base64_string):
+    url = f'https://generativelanguage.googleapis.com/v1/models/gemini-pro-vision:generateContent?key={GEMINI_API_KEY}'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "contents": [
+            {
+                "parts": [
+                    {"text": text},
+                    {
+                        "inline_data": {
+                            "mime_type": "image/jpeg",
+                            "data": image_base64_string
+                        }
+                    }
+                ]
+            },
+        ]
+    }
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+      return response.json()
+    else:
+      print(response.json())
       return "Error"
 
 app = Flask(__name__)
@@ -72,6 +98,23 @@ def handle_message(event):
     else:
       reply_message = reply_message["candidates"][0]["content"]["parts"][0]["text"]
   line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+    # 獲取圖片訊息的內容
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+    image_data = message_content.content
+    image_base64 = base64.b64encode(image_data).decode('utf-8')
+    reply_message = calling_gemini_vision_api("用繁體中文描述這張圖片", image_base64)
+
+    if reply_message == "Error":
+        reply_message = "發生錯誤，請稍後再試"
+    else:
+      print(reply_message)
+      reply_message = reply_message["candidates"][0]["content"]["parts"][0]["text"]
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
